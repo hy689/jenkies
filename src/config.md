8ee9411bbf984b8ff705bc0574c294993b60b4b0
hy <976849662@qq.com>
2024-09-05 16:22:38 +0800

    新增md

---
+++ src/config.md
@@ -0,0 +1,18 @@
+sso
+    qa-sso-olading-front
+    deploy-lanmaoly-cloud-sso-olading-front
+    lanmaoly-cloud-sso-olading-front
+
+salary
+    qa-salary-front
+    deploy-lanmaoly-cloud-salary-front
+    lanmaoly-cloud-salary-front
+    
+hro-salary
+    qa-lanmaoly-cloud-hro-salary-front
+    deploy-lanmaoly-cloud-hro-salary-front
+    lanmaoly-cloud-hro-salary-front
+
+
+
+
--- src/config.ts
+++ src/config.ts
@@ -1,40 +1,6 @@
 import { ITask } from './types'
 
 export const taskQueue: ITask[] = [
-    //  {
-    //     project: 'deploy-lanmaoly-cloud-hro-salary-front',
-    //     environment: 'deploy'
-    // },
-    // {
-    //     project: 'deploy-olading-hrsaas-control-front',
-    //     environment: 'deploy',
-    //     gitOptions: {
-    //         env: '46',
-    //         branch:'master'
-    //     }
-    // }, 
-    // {
-    //     project: 'qa-sso-olading-front',
-    //     environment: 'qa'
-    // },
-    // {
-    //     project: 'deploy-lanmaoly-cloud-sso-olading-front',
-    //     environment: 'deploy',
-    //     gitOptions: {
-    //         env: '114',
-    //         branch:'cgb/2.34'
-    //     }
-    // },  
-    // {
-    //     project: 'qa-salary-front',
-    //     environment: 'qa'
-    // },
-    // {
-    //     project: 'deploy-lanmaoly-cloud-salary-front',
-    //     environment: 'deploy'
-    // },
-
-
     {
         project: 'deploy-olading-contract-front',
         environment: 'deploy',
